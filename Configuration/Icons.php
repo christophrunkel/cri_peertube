256453c903aefd295340a3636c17d015c171337a
christophrunkel <32742371+christophrunkel@users.noreply.github.com>
2026-02-06 14:31:42 +0100

    Update Icons.php

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -1,1 +1,7 @@
-
+<?php
+return [
+    'mimetypes-media-image-peertube' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:cri_peertube/Resources/Public/Icons/Extension.svg',
+    ],
+];
